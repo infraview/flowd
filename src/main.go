06e32b55aa2677f1541f8784f722b1830bfd65ee
Marius Cotofana <marius.coto@gmail.com>
2017-04-13 13:59:55 +0300

    Print array of maps instead of an array of strings

--- src/main.go
+++ src/main.go
@@ -8,13 +8,39 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type FlowTable struct {
-	addressMap map[string]string
+	addressList []map[string]string
 }
 
+
 var flowTable *FlowTable
+
+func Extend(slice []map[string]string, element map[string]string) []map[string]string {
+    n := len(slice)
+    if n == cap(slice) {
+        // Slice is full; must grow.
+        // We double its size and add 1, so if the size is zero we still grow.
+        newSlice := make([]map[string]string, len(slice), 2*len(slice)+1)
+        copy(newSlice, slice)
+        slice = newSlice
+    }
+    slice = slice[0 : n+1]
+    slice[n] = element
+    return slice
+}
+
+
+// Append appends the items to the slice.
+// First version: just loop calling Extend.
+func Append(slice []map[string]string, items ...map[string]string) []map[string]string {
+    for _, item := range items {
+        slice = Extend(slice, item)
+    }
+    return slice
+}
 
 func runNetworkAnalyzer(networkInterface string) {
 	if handle, err := pcap.OpenLive(networkInterface, 1600, true, pcap.BlockForever); err != nil {
@@ -34,21 +60,27 @@ func runNetworkAnalyzer(networkInterface string) {
 
 				srcString := ip.SrcIP.String() + ":" + srcPort
 				dstString := ip.DstIP.String() + ":" + dstPort
-				flowTable.addressMap[srcString] = dstString
+
+				conn := map[string]string{
+					"t": strconv.FormatInt(time.Now().Unix(), 10),
+					"s": srcString,
+					"d": dstString,
+				}
+				flowTable.addressList = Append(flowTable.addressList, conn)
 			}
 		}
 	}
 }
 
 func IndexHandler(w http.ResponseWriter, req *http.Request) {
-	data, _ := json.Marshal(flowTable.addressMap)
+	data, _ := json.Marshal(flowTable.addressList)
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, string(data))
 }
 
 func main() {
 	flowTable = &FlowTable{
-		map[string]string{},
+		[]map[string]string{},
 	}
 
 	go runNetworkAnalyzer("eth0")
